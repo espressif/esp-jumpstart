3e8199fd3d716da03796ba31a7a589dbf4f1ef22
Kedar Sovani <kedars@gmail.com>
2018-09-25 10:30:13 +0530

    Hello World!

---
+++ 1hello_world/main/app_main.c
@@ -0,0 +1,22 @@
+/* Hello World Example
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+void app_main()
+{
+    int i = 0;
+    while (1) {
+        printf("[%d] Hello world!\n", i);
+        i++;
+        vTaskDelay(5000 / portTICK_PERIOD_MS);
+    }
+}
