7ffb524153963e07c90b6471389e5d0b8b882e27
Kedar Sovani <kedars@gmail.com>
2018-10-04 15:37:12 +0530

    Support for remote control through cloud

---
+++ 5cloud/main/app_main.c
@@ -0,0 +1,113 @@
+/* Cloud Communication
+
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
+#include <stdio.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <freertos/event_groups.h>
+#include <esp_log.h>
+#include <esp_wifi.h>
+#include <esp_event_loop.h>
+
+#include <nvs_flash.h>
+#include <conn_mgr_prov.h>
+#include <conn_mgr_prov_mode_ble.h>
+#include <conn_mgr_prov_mode_softap.h>
+
+#include "app_priv.h"
+
+static const char *TAG = "app_main";
+
+/* Signal Wi-Fi events on this event-group */
+const int WIFI_CONNECTED_EVENT = BIT0;
+static EventGroupHandle_t wifi_event_group;
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
+{
+    conn_mgr_prov_event_handler(ctx, event);
+
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        ESP_LOGI(TAG, "Connected with IP Address:%s", ip4addr_ntoa(&event->event_info.got_ip.ip_info.ip));
+        xEventGroupSetBits(wifi_event_group, WIFI_CONNECTED_EVENT);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        ESP_LOGI(TAG, "Disconnected, connectin to the AP again...\n");
+        esp_wifi_connect();
+        break;
+    default:
+        break;
+    }
+    return ESP_OK;
+}
+
+static void wifi_init_sta()
+{
+    wifi_event_group = xEventGroupCreate();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+}
+
+static void get_device_service_name(char *service_name, size_t max)
+{
+    uint8_t eth_mac[6];
+    const char *ssid_prefix = "PROV_";
+    esp_wifi_get_mac(WIFI_IF_STA, eth_mac);
+    snprintf(service_name, max, "%s%02X%02X%02X",
+             ssid_prefix, eth_mac[3], eth_mac[4], eth_mac[5]);
+}
+
+void app_main()
+{
+    app_driver_init();
+    /* Initialise NVS partition */
+    esp_err_t ret = nvs_flash_init();
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "Failed to init NVS");
+        return;
+    }
+
+    /* Initialise TCP/IP and the event loop */
+    tcpip_adapter_init();
+    ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
+
+    bool provisioned = false;
+    if (conn_mgr_prov_is_provisioned(&provisioned) != ESP_OK) {
+        ESP_LOGE(TAG, "Error getting device provisioning state");
+        return;
+    }
+    if (! provisioned) {
+        ESP_LOGI(TAG, "Starting provisioning");
+        /* What is the Device Service Name that we want */
+        char service_name[12];
+        get_device_service_name(service_name, sizeof(service_name));
+        /* What is the Provisioning Type that we want: conn_mgr_prov_mode_softap or conn_mgr_prov_mode_ble */
+        conn_mgr_prov_t prov_type = conn_mgr_prov_mode_ble;
+        /* What is the security level that we want: 0 or 1 */
+        int security = 1;
+        /* Do we want a proof-of-possession: for now this has to be a string with length  > 0 */
+        const char *pop = "abcd1234";
+        /* What is the service key */
+        const char *service_key = "DEADCAFE";
+        conn_mgr_prov_start_provisioning(prov_type, security, pop, service_name, service_key);
+    } else {
+        ESP_LOGI(TAG, "Already provisioned, starting station");
+        /* Start the station */
+        wifi_init_sta();
+        //nvs_flash_erase();
+    }
+
+    /* Wait for Wi-Fi connection */
+    xEventGroupWaitBits(wifi_event_group, WIFI_CONNECTED_EVENT, false, true, portMAX_DELAY);
+    cloud_start();
+}
---
+++ 5cloud/main/component.mk
@@ -0,0 +1,6 @@
+#
+# "main" pseudo-component makefile.
+#
+# (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
+
+COMPONENT_EMBED_TXTFILES := certs/aws-root-ca.pem certs/certificate.pem.crt certs/private.pem.key
