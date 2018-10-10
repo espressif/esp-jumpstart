48923f6eab14cd9b3dbbac49cd7b71dbc408af28
Kedar Sovani <kedars@gmail.com>
2018-10-10 16:56:29 +0530

    Use the iot_button component

--- 2outlet/main/app_driver.c
+++ 2outlet/main/app_driver.c
@@ -9,43 +9,34 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/semphr.h>
 #include <freertos/task.h>
-#include <driver/gpio.h>
+#include <iot_button.h>
 
 #include "app_priv.h"
 
 /* This is the button that is used for toggling the output */
-#define BUTTON_GPIO    0
+#define BUTTON_GPIO          0
+#define BUTTON_ACTIVE_LEVEL  0
 /* This is the GPIO on which the output will be set */
 #define OUTPUT_GPIO    14
 #define DEBOUNCE_TIME  30
 
 static bool g_output_state;
-static void IRAM_ATTR push_btn_isr_handler(void* arg)
+static void push_btn_cb(void* arg)
 {
     static uint64_t previous;
     uint64_t current = xTaskGetTickCount();
     if ((current - previous) > DEBOUNCE_TIME) {
-        /* Printing in interrupts isn't a good practice. This is only so we have a visual feedback. */
-        ets_printf("Button Pressed\n");
         previous = current;
         app_driver_toggle_state();
     }
 }
 
-static void configure_push_button(int gpio_num, void (*isr_handler)(void *))
+static void configure_push_button(int gpio_num, void (*btn_cb)(void *))
 {
-    gpio_config_t io_conf = {
-        .intr_type = GPIO_PIN_INTR_POSEDGE,
-        .mode = GPIO_MODE_INPUT,
-        .pull_up_en = 1,
-    };
-    io_conf.pin_bit_mask = ((uint64_t)1 << gpio_num);
-    /* Configure the GPIO */
-    gpio_config(&io_conf);
-    //install gpio isr service
-    gpio_install_isr_service(0);
-    //hook isr handler for specific gpio pin
-    gpio_isr_handler_add(gpio_num, isr_handler, NULL);
+    button_handle_t btn_handle = iot_button_create(BUTTON_GPIO, BUTTON_ACTIVE_LEVEL);
+    if (btn_handle) {
+        iot_button_set_evt_cb(btn_handle, BUTTON_CB_RELEASE, btn_cb, "RELEASE");
+    }
 }
 
 static void set_output_state(bool target)
@@ -55,7 +46,7 @@ static void set_output_state(bool target)
 
 void app_driver_init()
 {
-    configure_push_button(BUTTON_GPIO, push_btn_isr_handler);
+    configure_push_button(BUTTON_GPIO, push_btn_cb);
 
     /* Configure output */
     gpio_config_t io_conf = {
