ba26553d6dc9a648632974a3f85cd9e715c103b6
Anuj Deshpande <anuj@espressif.com>
2019-03-04 14:17:14 +0530

    Remove CI cloud configuration from component.mk

--- 5_cloud/main/component.mk
+++ 5_cloud/main/component.mk
@@ -4,16 +4,3 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 COMPONENT_EMBED_TXTFILES := cloud_cfg/server.cert cloud_cfg/device.cert cloud_cfg/device.key cloud_cfg/deviceid.txt cloud_cfg/endpoint.txt
-
-ifndef IDF_CI_BUILD
-# Print an error if the certificate/key files are missing
-$(COMPONENT_PATH)/cloud_cfg/server.cert $(COMPONENT_PATH)/cloud_cfg/device.cert $(COMPONENT_PATH)/cloud_cfg/device.key $(COMPONENT_PATH)/cloud_cfg/deviceid.txt $(COMPONENT_PATH)/cloud_cfg/endpoint.txt:
-	@echo "Missing file $@. This file identifies the ESP32 to cloud."
-	exit 1
-else  # IDF_CI_BUILD
-# this case is for the internal Continuous Integration build which
-# compiles all examples. Add some dummy certs so the example can
-# compile (even though it won't work)
-$(COMPONENT_PATH)/cloud_cfg/server.cert $(COMPONENT_PATH)/cloud_cfg/device.cert $(COMPONENT_PATH)/cloud_cfg/device.key $(COMPONENT_PATH)/cloud_cfg/deviceid.txt $(COMPONENT_PATH)/cloud_cfg/endpoint.txt:
-	echo "Dummy certificate data for continuous integration" > $@
-endif
--- 7_mfg/main/component.mk
+++ 7_mfg/main/component.mk
@@ -4,17 +4,3 @@
 # (Uses default behaviour of compiling all source files in directory, adding 'include' to include path.)
 
 COMPONENT_EMBED_TXTFILES := cloud_cfg/endpoint.txt cloud_cfg/server.cert cloud_cfg/github_server.cert
-
-
-ifndef IDF_CI_BUILD
-# Print an error if the certificate/key files are missing
-$(COMPONENT_PATH)/cloud_cfg/server.cert $(COMPONENT_PATH)/cloud_cfg/endpoint.txt $(COMPONENT_PATH)/cloud_cfg/github_server.cert:
-	@echo "Missing file $@. This file identifies the ESP32 to cloud."
-	exit 1
-else  # IDF_CI_BUILD
-# this case is for the internal Continuous Integration build which
-# compiles all examples. Add some dummy certs so the example can
-# compile (even though it won't work)
-$(COMPONENT_PATH)/cloud_cfg/server.cert $(COMPONENT_PATH)/cloud_cfg/endpoint.txt $(COMPONENT_PATH)/cloud_cfg/github_server.cert:
-	echo "Dummy certificate data for continuous integration" > $@
-endif
