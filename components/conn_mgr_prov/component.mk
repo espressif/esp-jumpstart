518d3270588c32101e3cb7c26b58cadd904085d0
Anurag Kar <anurag.kar@espressif.com>
2019-07-11 10:01:02 +0530

    conn_mgr_prov : conn_mgr_prov_mode_ble.c compiled only when BLE is enabled

--- components/conn_mgr_prov/component.mk
+++ components/conn_mgr_prov/component.mk
@@ -1,3 +1,7 @@
 COMPONENT_SRCDIRS := src
 COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := src
+
+ifneq ($(filter y, $(CONFIG_BT_ENABLED) $(CONFIG_BLUEDROID_ENABLED)),y y)
+	COMPONENT_OBJEXCLUDE := src/conn_mgr_prov_mode_ble.o
+endif
