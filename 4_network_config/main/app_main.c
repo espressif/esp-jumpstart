7f7c6d5bcda639a46a8637a569436abe87d3d356
Piyush Shah <piyush@espressif.com>
2019-09-10 15:13:46 +0530

    wifi_provisioning: Use the new Wi-Fi provisioning APIs for IDF 3.3

--- 4_network_config/main/app_main.c
+++ 4_network_config/main/app_main.c
@@ -7,6 +7,7 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <stdio.h>
+#include <string.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <esp_log.h>
@@ -14,9 +15,9 @@
 #include <esp_event_loop.h>
 
 #include <nvs_flash.h>
-#include <conn_mgr_prov.h>
-#include <conn_mgr_prov_mode_ble.h>
-#include <conn_mgr_prov_mode_softap.h>
+#include <wifi_provisioning/manager.h>
+#include <wifi_provisioning/scheme_ble.h>
+#include <wifi_provisioning/scheme_softap.h>
 
 #include "app_priv.h"
 
@@ -26,7 +27,7 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
     /* Pass event information to conn_mgr_prov so that it can
      * maintain it's internal state depending upon the system event */
-    conn_mgr_prov_event_handler(ctx, event);
+    wifi_prov_mgr_event_handler(ctx, event);
 
     /* Global event handler */
     switch (event->event_id) {
@@ -46,26 +47,47 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-/* Custom event handler for catching provisioning manager events */
-static esp_err_t prov_event_handler(void *user_data, conn_mgr_prov_cb_event_t event)
+/* Event handler for catching provisioning manager events */
+static void prov_event_handler(void *user_data,
+                               wifi_prov_cb_event_t event, void *event_data)
 {
-    esp_err_t ret = ESP_OK;
     switch (event) {
-        case CMP_PROV_END:
-            /* De-initialize manager once
-             * provisioning is finished */
-            conn_mgr_prov_deinit();
+        case WIFI_PROV_START:
+            ESP_LOGI(TAG, "Provisioning started");
+            break;
+        case WIFI_PROV_CRED_RECV: {
+            wifi_sta_config_t *wifi_sta_cfg = (wifi_sta_config_t *)event_data;
+            /* If SSID length is exactly 32 bytes, null termination
+             * will not be present, so explicitly obtain the length */
+            size_t ssid_len = strnlen((const char *)wifi_sta_cfg->ssid, sizeof(wifi_sta_cfg->ssid));
+            ESP_LOGI(TAG, "Received Wi-Fi credentials"
+                     "\n\tSSID     : %.*s\n\tPassword : %s",
+                     ssid_len, (const char *) wifi_sta_cfg->ssid,
+                     (const char *) wifi_sta_cfg->password);
+            break;
+        }
+        case WIFI_PROV_CRED_FAIL: {
+            wifi_prov_sta_fail_reason_t *reason = (wifi_prov_sta_fail_reason_t *)event_data;
+            ESP_LOGE(TAG, "Provisioning failed!\n\tReason : %s"
+                     "\n\tPlease reset to factory and retry provisioning",
+                     (*reason == WIFI_PROV_STA_AUTH_ERROR) ?
+                     "Wi-Fi AP password incorrect" : "Wi-Fi AP not found");
+            break;
+        }
+        case WIFI_PROV_CRED_SUCCESS:
+            ESP_LOGI(TAG, "Provisioning successful");
+            break;
+        case WIFI_PROV_END:
+            /* De-initialize manager once provisioning is finished */
+            wifi_prov_mgr_deinit();
             break;
         default:
             break;
     }
-    return ret;
 }
 
 static void wifi_init_sta()
 {
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
     ESP_ERROR_CHECK(esp_wifi_start() );
 }
@@ -102,11 +124,15 @@ void app_main()
     tcpip_adapter_init();
     ESP_ERROR_CHECK(esp_event_loop_init(event_handler, NULL) );
 
+    /* Initialize Wi-Fi */
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
     /* Configuration for the provisioning manager */
-    conn_mgr_prov_config_t config = {
+    wifi_prov_mgr_config_t config = {
         /* What is the Provisioning Scheme that we want ?
-         * conn_mgr_prov_scheme_softap or conn_mgr_prov_scheme_ble */
-        .scheme = conn_mgr_prov_scheme_ble,
+         * wifi_prov_scheme_softap or wifi_prov_scheme_ble */
+        .scheme = wifi_prov_scheme_ble,
 
         /* Any default scheme specific event handler that you would
          * like to choose. Since our example application requires
@@ -115,8 +141,8 @@ void app_main()
          * (in case when device is already provisioned). Choosing
          * appropriate scheme specific event handler allows the manager
          * to take care of this automatically. This can be set to
-         * CMP_EVENT_HANDLER_NONE when using conn_mgr_prov_scheme_softap*/
-        .scheme_event_handler = CMP_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM,
+         * WIFI_PROV_EVENT_HANDLER_NONE when using wifi_prov_scheme_softap*/
+        .scheme_event_handler = WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BTDM,
 
         /* Do we want an application specific handler be executed on
          * various provisioning related events */
@@ -128,17 +154,11 @@ void app_main()
 
     /* Initialize provisioning manager with the
      * configuration parameters set above */
-    if (conn_mgr_prov_init(config) != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to initialize provisioning manager");
-        return;
-    }
+    ESP_ERROR_CHECK(wifi_prov_mgr_init(config));
 
     bool provisioned = false;
     /* Let's find out if the device is provisioned */
-    if (conn_mgr_prov_is_provisioned(&provisioned) != ESP_OK) {
-        ESP_LOGE(TAG, "Error getting device provisioning state");
-        return;
-    }
+    ESP_ERROR_CHECK(wifi_prov_mgr_is_provisioned(&provisioned));
 
     /* If device is not yet provisioned start provisioning service */
     if (!provisioned) {
@@ -146,19 +166,19 @@ void app_main()
 
         /* What is the Device Service Name that we want
          * This translates to :
-         *     - WiFi SSID when scheme is conn_mgr_prov_scheme_softap
-         *     - device name when scheme is conn_mgr_prov_scheme_ble
+         *     - Wi-Fi SSID when scheme is wifi_prov_scheme_softap
+         *     - device name when scheme is wifi_prov_scheme_ble
          */
         char service_name[12];
         get_device_service_name(service_name, sizeof(service_name));
 
         /* What is the security level that we want (0 or 1):
-         *      - Security 0 is simply plain text communication.
-         *      - Security 1 is secure communication which consists of secure handshake
+         *      - WIFI_PROV_SECURITY_0 is simply plain text communication.
+         *      - WIFI_PROV_SECURITY_1 is secure communication which consists of secure handshake
          *          using X25519 key exchange and proof of possession (pop) and AES-CTR
          *          for encryption/decryption of messages.
          */
-        int security = 1;
+        wifi_prov_security_t security = WIFI_PROV_SECURITY_1;
 
         /* Do we want a proof-of-possession (ignored if Security 0 is selected):
          *      - this should be a string with length > 0
@@ -168,27 +188,44 @@ void app_main()
 
         /* What is the service key (could be NULL)
          * This translates to :
-         *     - WiFi password when scheme is conn_mgr_prov_scheme_softap
-         *     - simply ignored when scheme is conn_mgr_prov_scheme_ble
+         *     - Wi-Fi password when scheme is wifi_prov_scheme_softap
+         *     - simply ignored when scheme is wifi_prov_scheme_ble
          */
         const char *service_key = NULL;
 
+        /* This step is only useful when scheme is wifi_prov_scheme_ble. This will
+         * set a custom 128 bit UUID which will be included in the BLE advertisement
+         * and will correspond to the primary GATT service that provides provisioning
+         * endpoints as GATT characteristics. Each GATT characteristic will be
+         * formed using the primary service UUID as base, with different auto assigned
+         * 12th and 13th bytes (assume counting starts from 0th byte). The client side
+         * applications must identify the endpoints by reading the User Characteristic
+         * Description descriptor (0x2901) for each characteristic, which contains the
+         * endpoint name of the characteristic */
+        uint8_t custom_service_uuid[] = {
+            /* LSB <---------------------------------------
+             * ---------------------------------------> MSB */
+            0x21, 0x43, 0x65, 0x87, 0x09, 0xba, 0xdc, 0xfe,
+            0xef, 0xcd, 0xab, 0x90, 0x78, 0x56, 0x34, 0x12
+        };
+        wifi_prov_scheme_ble_set_service_uuid(custom_service_uuid);
+
         /* Start provisioning service */
-        conn_mgr_prov_start_provisioning(security, pop, service_name, service_key);
+        ESP_ERROR_CHECK(wifi_prov_mgr_start_provisioning(security, pop, service_name, service_key));
 
         /* Uncomment the following to wait for the provisioning to finish and then release
          * the resources of the manager. Since in this case de-initialization is triggered
          * by the configured prov_event_handler(), we don't need to call the following */
-        // conn_mgr_prov_wait();
-        // conn_mgr_prov_deinit();
+        // wifi_prov_mgr_wait();
+        // wifi_prov_mgr_deinit();
     } else {
-        ESP_LOGI(TAG, "Already provisioned, starting station");
-
-        /* Start wifi station */
-        wifi_init_sta();
+        ESP_LOGI(TAG, "Already provisioned, starting Wi-Fi STA");
 
         /* We don't need the manager as device is already provisioned,
          * so let's release it's resources */
-        conn_mgr_prov_deinit();
+        wifi_prov_mgr_deinit();
+
+        /* Start Wi-Fi station */
+        wifi_init_sta();
     }
 }
