9333c7030680dc559e1fd5dd0dea7eea421f8dc0
Zhang Wei <zhang.wei@espressif.com>
2023-01-17 11:15:00 +0800

    Support ESP32C3-DevKit-m

--- 4_network_config/main/app_driver.c
+++ 4_network_config/main/app_driver.c
@@ -15,8 +15,13 @@
 
 #include "app_priv.h"
 #include JUMPSTART_BOARD
+#ifdef CONFIG_IDF_TARGET_ESP32C3
+#include "driver/rmt.h"
+#include "led_strip.h"
+static led_strip_t *g_strip_handle = NULL;
+#endif
 
-static bool g_output_state;
+static bool g_output_state = false;
 static void push_btn_cb(void *arg)
 {
     app_driver_set_state(!g_output_state);
@@ -39,13 +44,23 @@ static void configure_push_button(int gpio_num, void (*btn_cb)(void *))
 
 static void set_output_state(bool target)
 {
+#ifdef CONFIG_IDF_TARGET_ESP32
     gpio_set_level(JUMPSTART_BOARD_OUTPUT_GPIO, target);
+#elif CONFIG_IDF_TARGET_ESP32C3
+    if (target) {
+        g_strip_handle->set_pixel(g_strip_handle, 0, 255, 255, 255);
+        g_strip_handle->refresh(g_strip_handle, 100);
+    } else {
+        g_strip_handle->clear(g_strip_handle, 100);
+    }
+#endif
 }
 
 void app_driver_init()
 {
     configure_push_button(JUMPSTART_BOARD_BUTTON_GPIO, push_btn_cb);
 
+#ifdef CONFIG_IDF_TARGET_ESP32
     /* Configure output */
     gpio_config_t io_conf = {
         .mode = GPIO_MODE_OUTPUT,
@@ -54,6 +69,10 @@ void app_driver_init()
     io_conf.pin_bit_mask = ((uint64_t)1 << JUMPSTART_BOARD_OUTPUT_GPIO);
     /* Configure the GPIO */
     gpio_config(&io_conf);
+#elif CONFIG_IDF_TARGET_ESP32C3
+    g_strip_handle = led_strip_init(RMT_CHANNEL_0, JUMPSTART_BOARD_OUTPUT_GPIO, 1);
+    app_driver_set_state(!g_output_state);
+#endif
 }
 
 int IRAM_ATTR app_driver_set_state(bool state)
